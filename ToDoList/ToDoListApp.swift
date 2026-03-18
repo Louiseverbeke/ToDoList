e7e42162f47d733330e967b7e6d176d8804bd14e
Louise Verbeke <louiseverbeke@yahoo.be>
2026-03-18 15:32:24 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Louise Verbeke on 18/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
