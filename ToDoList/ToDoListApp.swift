755e40c27717695e337cbe6b5eb3da450e13acc6
Louise Verbeke <louiseverbeke@yahoo.be>
2026-03-22 15:25:37 -0400

    after 5.5 ToDoList

--- ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoListApp.swift
@@ -6,12 +6,19 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct ToDoListApp: App {
     var body: some Scene {
         WindowGroup {
             ToDoListView()
+                .modelContainer(for: ToDo.self)
         }
+    }
+    
+    //will allow us to find where our simulator datea is saved:
+    init() {
+        print(URL.applicationSupportDirectory.path(percentEncoded: false))
     }
 }
